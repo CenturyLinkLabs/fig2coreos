dc8cc7057ad597c73bce4b188fc127258a9ab6cb
Ozzie Gooen <Ozzie@grandroundshealth.com>
2014-12-04 14:00:03 -0800

    additional files for testing

--- spec/fig2coreos_spec.rb
+++ spec/fig2coreos_spec.rb
@@ -1,12 +1,22 @@
 require 'spec_helper'
+require 'pathname'
 
 describe Fig2CoreOS do
-  it 'does stuff' do
-    expect {Fig2CoreOS.convert(
-      'serf',
-      'spec/fig.yml',
-      '/tmp',
-      type: 'fleet', skip_discovery_file: true
-    )}.not_to raise_error
+  describe '#convert' do
+    context 'without discovery files' do
+      let(:expected_files) { Dir['spec/support/sample/output/*.service'].map{|f| Pathname.new(f).basename}}
+      before(:all) do
+        Fig2CoreOS.convert(
+          'serf',
+          'spec/support/sample/input.yml',
+          '/tmp',
+          type: 'fleet', skip_discovery_file: true
+        )
+      end
+
+      it 'creates correct service files' do
+        expected_files
+      end
+    end
   end
 end
